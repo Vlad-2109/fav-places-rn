e48f5934b1a361a14d7ebcd614f413c8cb4e37f7
clouden <clouden@syneforge.com>
2026-09-06 21:33:46 +0300

    add a header button

--- src/app/(home)/index.tsx
+++ src/app/(home)/index.tsx
@@ -3,15 +3,26 @@ import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import PlacesList from '@/components/Places/PlacesList';
+import IconButton from '@/components/UI/IconButton';
 
 const AllPlacesScreen = () => {
 	const router = useRouter();
 
-	// router.push(`/add-place`);
-
 	return (
 		<SafeAreaView style={styles.container}>
-			<Stack.Screen options={{ title: 'All Places' }} />
+			<Stack.Screen
+				options={{
+					title: 'All Places',
+					headerRight: ({ tintColor }) => (
+						<IconButton
+							icon="add"
+							size={24}
+							color={tintColor as string}
+							onPress={() => router.push(`/add-place`)}
+						/>
+					),
+				}}
+			/>
 			<PlacesList places={[]} />
 		</SafeAreaView>
 	);
---
+++ src/components/UI/IconButton.tsx
@@ -0,0 +1,34 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Pressable, StyleSheet } from 'react-native';
+
+type IconButtonProps = {
+	icon: keyof typeof Ionicons.glyphMap;
+	size: number;
+	color: string;
+	onPress: () => void;
+};
+
+const IconButton = ({ icon, size, color, onPress }: IconButtonProps) => {
+	return (
+		<Pressable
+			onPress={onPress}
+			style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+		>
+			<Ionicons name={icon} size={size} color={color} />
+		</Pressable>
+	);
+};
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+	button: {
+		padding: 8,
+		margin: 4,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	pressed: {
+		opacity: 0.7,
+	},
+});
