85626047541d3fbacbb6700c3ff42e44a9eb22a9
clouden <clouden@syneforge.com>
2026-09-11 21:11:22 +0300

    all map interaction and adding markers

--- src/app/map/index.tsx
+++ src/app/map/index.tsx
@@ -1,9 +1,20 @@
 import { Stack } from 'expo-router';
+import { useState } from 'react';
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker, type MapPressEvent } from 'react-native-maps';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+type Location = {
+	lat: number;
+	lng: number;
+};
+
 const MapScreen = () => {
+	const [selectedLocation, setSelectedLocation] = useState<Location>({
+		lat: 0,
+		lng: 0,
+	});
+
 	const region = {
 		latitude: 37.78,
 		longitude: -122.43,
@@ -11,10 +22,29 @@ const MapScreen = () => {
 		longitudeDelta: 0.0421,
 	};
 
+	const handleSelectLocation = (event: MapPressEvent) => {
+		const lat = event.nativeEvent.coordinate.latitude;
+		const lng = event.nativeEvent.coordinate.longitude;
+
+		setSelectedLocation({ lat, lng });
+	};
+
 	return (
 		<SafeAreaView style={styles.container} edges={['left', 'right']}>
 			<Stack.Screen options={{ title: 'Map' }} />
-			<MapView initialRegion={region} style={styles.map}></MapView>
+			<MapView
+				initialRegion={region}
+				style={styles.map}
+				onPress={handleSelectLocation}
+			>
+				<Marker
+					title="Picked Location"
+					coordinate={{
+						latitude: selectedLocation.lat,
+						longitude: selectedLocation.lng,
+					}}
+				/>
+			</MapView>
 		</SafeAreaView>
 	);
 };
