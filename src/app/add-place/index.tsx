f1406f3326e871735a9c3f166d8bab9377b9d8c2
clouden <clouden@syneforge.com>
2026-09-17 20:42:36 +0300

    inserting places into the database

--- src/app/add-place/index.tsx
+++ src/app/add-place/index.tsx
@@ -3,13 +3,15 @@ import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import type { PlaceModel } from '@/models/place';
+import { insertPlace } from '@/utils/database';
 
 import PlaceForm from '@/components/Places/PlaceForm';
 
 const AddPlaceScreen = () => {
 	const router = useRouter();
 
-	const handleCreatePlace = (place: PlaceModel) => {
+	const handleCreatePlace = async (place: PlaceModel) => {
+		await insertPlace(place);
 		router.push({
 			pathname: '/',
 			params: {
